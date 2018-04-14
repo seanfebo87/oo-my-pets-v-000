df7f7ac48ea1ed1fdc9841ed2b6697f16c4a9cf6
Sean Febo <seanfebo@techie.com>
2018-04-14 20:52:06 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -53,9 +53,7 @@ class Owner
  end
  
  def sell_pets
-   @pets[:dogs].each{|dog|dog.mood ="nervous"}
-   @pets[:cats].each{|cat|cat.mood ="nervous"}
-   @pets[:fishes].each{|fish|fish.mood ="nervous"}
+   
    @pets.clear
  end
 
